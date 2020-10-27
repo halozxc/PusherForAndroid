c264b38484f12352524674670d79dc16bdad4460
halozxc <40384526+halozxc@users.noreply.github.com>
2020-10-27 18:34:41 +0800

    camer call

--- app/src/main/java/com/example/pusher/PublicationImageActivity.java
+++ app/src/main/java/com/example/pusher/PublicationImageActivity.java
@@ -1,14 +1,80 @@
 package com.example.pusher;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Debug;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 
 public class PublicationImageActivity extends AppCompatActivity {
-
+public static final int TAKE_PHOTO =1;
+    ImageView publicImage ;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions((Activity) this, new String[]{Manifest.permission.CAMERA}, 100);
+        }
         setContentView(R.layout.activity_publication_image);
+        publicImage =findViewById(R.id.ivpublicationContent);
+        publicImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File outputImage = new File(getExternalCacheDir(),"output_image.jpg");
+                try{
+                    if(outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+                if(Build.VERSION.SDK_INT>=24){
+                    imageUri = FileProvider.getUriForFile(PublicationImageActivity.this,"com.example.cameraalbumtest.fileprovider",outputImage);
+                }else{
+                    imageUri =Uri.fromFile(outputImage);
+                }
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,TAKE_PHOTO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (resultCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Glide.with(this).load(imageUri).into(publicImage);
+                }
+                break;
+            default:
+                Log.d("get camera","no image captured");
+                break;
+        }
     }
 }
