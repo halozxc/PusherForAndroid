3cb211aebf8f07091994d12317066400cc9a88bd
halozxc <824927872@qq.com>
2020-12-11 09:28:20 +0800

    、、、、

---
+++ app/src/main/java/com/example/pusher/vipactivity.java
@@ -0,0 +1,21 @@
+package com.example.pusher;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class vipactivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vipactivity);
+
+
+        // Enables Always-on
+
+    }
+}
